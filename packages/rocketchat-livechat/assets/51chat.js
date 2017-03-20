7e659c760a47336aeee590099a015988f1ff09dc
dale <dale.guo@loweprofero.com>
2017-03-20 11:51:40 +0800

    add TODO: resize & javascript animate

--- packages/rocketchat-livechat/assets/51chat.js
+++ packages/rocketchat-livechat/assets/51chat.js
@@ -229,8 +229,12 @@
 		});
     widget = document.querySelector('.rocketchat-widget');
     iframe = document.getElementById('rocketchat-iframe');
-
-    w.addEventListener('message', function(msg) {
+		window.addEventListener('resize', function(event){
+			if(document.documentElement.clientWidth<768){
+				console.log('TODO:// resized <768');
+			};
+		});
+		w.addEventListener('message', function(msg) {
       if (typeof msg.data === 'object' && msg.data.src !== undefined && msg.data.src === 'rocketchat') {
         if (api[msg.data.fn] !== undefined && typeof api[msg.data.fn] === 'function') {
           var args = [].concat(msg.data.args || []);
